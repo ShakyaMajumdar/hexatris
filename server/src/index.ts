5c634e5d1502e46a82b74e0f1a0f20f433c35cfd
Shakya Majumdar <shakyamajumdar1@gmail.com>
2023-11-12 12:08:19 +0530

    build script

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 const port = 80;
 
-app.use(express.static('../public'));
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
+  console.log("world")
+  console.log(__dirname)
   res.sendFile("index.html",);
 });
 
